cc574f60fc8b0ef487fc698e83ef95ce6d4fbbda
kangyou-0987432 <0987432@hr.nl>
2022-09-22 16:37:47 +0200

    A working window that close when u press esc

--- Trying_to_make_a_pygame_window.py
+++ Trying_to_make_a_pygame_window.py
@@ -4,25 +4,39 @@ import pygame
 # Initialize all imported pygame modules
 pygame.init()
 
-# Making a variable for the background and screensize
-SCREEN_WIDTH = 1024
-SCREEN_HEIGHT = 768
-BACKGROUND_COLOR = (0, 0, 0)
 
-# The name that will be displayed on the pygame
-pygame.display.set_caption("Werkplaats 1: PyGame")
+# Defining a function (Make a variable false, if u)
+def quit_game_requested():
+    stopping_game = False
+    keys = pygame.key.get_pressed()
+    for event in pygame.event.get():
+        # if i press ESC-button then the program will close.
+        if event.type == pygame.QUIT or keys[pygame.K_ESCAPE]:
+            stopping_game = True
+            break
+    return stopping_game
 
-# This function is used to create a clock object which can be used to keep track of time
-clock = pygame.time.Clock()
 
-# This Initialize a window or screen for display (How big u want the screen to be)
-canvas = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+# Making a game loop. While the The Stopping_game is not True, it keeps going to loop
+while not quit_game_requested():
+    SCREEN_WIDTH = 1024
+    SCREEN_HEIGHT = 768
+    BACKGROUND_COLOR = (85, 180, 176)
 
-# Gives u the option to change the background color
-canvas.fill(BACKGROUND_COLOR)
+    # The name that will be displayed on the pygame
+    pygame.display.set_caption("Werkplaats 1: PyGame")
 
-# Allows only a portion of the screen to updated, instead of the entire area
-pygame.display.flip()
+    # This function is used to create a clock object which can be used to keep track of time
+    clock = pygame.time.Clock()
+
+    # This Initialize a window or screen for display (How big u want the screen to be)
+    canvas = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # Gives u the option to change the background color
+    canvas.fill(BACKGROUND_COLOR)
+
+    # Allows only a portion of the screen to updated, instead of the entire area
+    pygame.display.flip()
 
 # It runs code that deactivates the Pygame library ( kind of the opposite of pygame.init() )
 pygame.quit()
