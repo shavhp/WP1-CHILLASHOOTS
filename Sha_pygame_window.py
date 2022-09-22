94827ba62ad304a92cd4a2690c5745f539eec235
Sharelle-0825486 <0825486@hr.nl>
2022-09-22 16:18:34 +0200

    Closes window also with ESC-key

--- Sha_pygame_window.py
+++ Sha_pygame_window.py
@@ -6,8 +6,13 @@ pygame.init()
 # Create screen
 SCREEN = pygame.display.set_mode((1024, 768))
 
+# Main event loop
 running = True
 while running:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        if event.type == pygame.KEYDOWN:
+            event.key = pygame.K_ESCAPE
+            running = False
+
+        elif event.type == pygame.QUIT:
             running = False
