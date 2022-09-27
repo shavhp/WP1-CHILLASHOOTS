5977dc4ece85c14f083b1d7f33183a36271cfc96
Sharelle-0825486 <0825486@hr.nl>
2022-09-27 14:38:57 +0200

    Removes a comment

--- Sha_pygame_window.py
+++ Sha_pygame_window.py
@@ -7,7 +7,7 @@ pygame.init()
 SCREEN_SIZE = pygame.display.set_mode((1024, 768))
 
 # Title and Icon
-# Reminder to attribute creator of icon in credit section of game
+
 pygame.display.set_caption("ChillaShoots")
 icon = pygame.image.load('chinchilla_icon_sha.png')
 pygame.display.set_icon(icon)
