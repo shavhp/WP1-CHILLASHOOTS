f905271094e2e89e98dd01e970659fc8f1e82580
kangyou-0987432 <0987432@hr.nl>
2022-10-03 14:44:20 +0200

    Adding a button to the screen that works

--- Making_a_setting_Menu.py
+++ Making_a_setting_Menu.py
@@ -4,6 +4,12 @@ import pygame
 # Initialize all imported pygame modules
 pygame.init()
 
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+# This Initialize a window or screen for display (How big u want the screen to be)
+canvas = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
 
 # Defining a function (Make a variable false, if u)
 def quit_game_requested():
@@ -16,29 +22,42 @@ def quit_game_requested():
             break
     return stopping_game
 
+
+# Define fonts
+font = pygame.font.SysFont("arialblack", 40)
+
+# Define colours
+TEXT_COL = (255, 255, 255)
+
+
 # Making a text function
 def draw_text(text, font, text_col, x, y):
     img = font.render(text, True, text_col)
     canvas.blit(img, (x, y))
 
 
+# Game variables
+game_paused = False
+
 # Making a game loop. While the The Stopping_game is not True, it keeps going to loop
 while not quit_game_requested():
-    SCREEN_WIDTH = 1024
-    SCREEN_HEIGHT = 768
-    BACKGROUND_COLOR = (0, 0, 0)
+
+    # Check if game is paused
+    if game_paused == True:
+        draw_text("HELLO", font, TEXT_COL, 160, 250)
+        # Display menu
+    else:
+        draw_text("Press SPACE to pause", font, TEXT_COL, 160, 250)
 
     # The name that will be displayed on the pygame
     pygame.display.set_caption("Werkplaats 1: PyGame")
 
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                game_paused = True
     # This function is used to create a clock object which can be used to keep track of time
     clock = pygame.time.Clock()
-
-    # This Initialize a window or screen for display (How big u want the screen to be)
-    canvas = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
-    # Gives u the option to change the background color
-    canvas.fill(BACKGROUND_COLOR)
 
     # Allows only a portion of the screen to updated, instead of the entire area
     pygame.display.flip()
