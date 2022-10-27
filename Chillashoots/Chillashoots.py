7a3691109b8e58a1261d065de56bc3e0bdee91a9
dennis-1031879 <1031879@hr.nl>
2022-10-27 19:37:16 +0200

    Update: Code added to main file.

--- Chillashoots/Chillashoots.py
+++ Chillashoots/Chillashoots.py
@@ -1,58 +1,61 @@
+import pygame.sprite
 from lib.Button import *
 from lib.Setting_menu import *
 from lib.Enemy import *
 from lib.Moving_Background_1 import *
 from lib.Sound import sound_maker
 from lib.Player import *
-# from lib.Title import *
 from lib.Setting_menu import setting_page
 import os
 
-# Initializes pygame library
 pygame.init()
+
+WIDTH = 800
+HEIGHT = 600
+FPS = 60
+
+GREY = (200, 200, 200)
 
 # Make button
 start_img = pygame.image.load(os.path.join('../images', 'button_start.png')).convert_alpha()
 more_img = pygame.image.load(os.path.join('../images', 'button_more.png')).convert_alpha()
+
 start_button = Button(250, 300, start_img, 1)
 more_button = Button(460, 430, more_img, 1)
-
-# Define colors
-GREY = (200, 200, 200)
-
-
-bulletImg = pygame.image.load('../images/bullet.png')
-bulletX = player_x
-bulletY = player_y
-bulletX_change = 50
-bulletY_change = 0
-bullet_state = "ready"
-
-# Used to manage how fast the screen updates
-clock = pygame.time.Clock()
-
-# Enemy spawn timers
-enemy_timer = 0
-bouncer_enemy_timer = 0
-
 font_score = pygame.font.Font('../fonts/superstar_memesbruh03.ttf', 25)
 
-# Game speed and frame counter
+high_score = 0
+high_score_file = open("../high_score.txt", "r")
+high_score = int(high_score_file.read())
+high_score_file.close()
+
+HIGH_SCORE_FONT = pygame.font.Font('../fonts/superstar_memesbruh03.ttf', 28)
+HIGH_SCORE = HIGH_SCORE_FONT.render(f'Highscore:{high_score}', True, GREY, None)
+
+HIGH_SCORE_RECT = HIGH_SCORE.get_rect()
+HIGH_SCORE_RECT.center = (SCREEN_WIDTH // 2.3, SCREEN_HEIGHT // 1.30)
+
+# define colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+GREY = (200, 200, 200)
+
+pygame.init()
+screen = pygame.display.set_mode((800, 600))
+enemy_timer = 0
+bouncer_enemy_timer = 0
+enemySprites = pygame.sprite.Group()
 frame_count = 0
 frame_rate = 60
 start_time = 0
+player_speed = 20
 
 # Calculate total seconds
 total_seconds = frame_count // frame_rate
-
-# For infinite enemy spawning
-global enemySprites
-enemySprites = pygame.sprite.RenderPlain(())
-# Pre-places an enemy, speed can be modified (x,y)
-enemySprites.add(Upper(10))
-enemySprites.add(Lower(10))
-
-
 
 
 def get_high_score():
@@ -64,7 +67,6 @@ def get_high_score():
         high_score_file = open("../high_score.txt", "r")
         high_score = int(high_score_file.read())
         high_score_file.close()
-        print (high_score)
 
     except IOError:
         # Error reading file, no high score
@@ -75,6 +77,7 @@ def get_high_score():
 
     return high_score
 
+
 def save_high_score(new_high_score):
     try:
         # Write file to disk
@@ -84,6 +87,7 @@ def save_high_score(new_high_score):
     except IOError:
         # Can't write it
         print("Unable to save high score.")
+
 
 def high_score_main():
     ''' Main program here '''
@@ -99,17 +103,141 @@ def high_score_main():
         save_high_score(current_score)
 
 
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Enemy, self).__init__()
+        self.surf = pygame.image.load(os.path.join('../images', 'ghost.png'))
+        # The starting position is randomly generated, as is the speed
+        self.rect = self.surf.get_rect(
+            center=(
+                random.randint(SCREEN_WIDTH + 20, SCREEN_WIDTH + 100),
+                random.randint(0, SCREEN_HEIGHT),
+            )
+        )
+        self.speed = random.randint(5, 20)
 
-def fire_bullet(x, y):
-    global bullet_state
-    bullet_state = "fire"
-    CANVAS.blit(bulletImg, (x + 50, y + 10 ))
+    # Move the enemy based on speed
+    # Remove it when it passes the left edge of the screen
+    def update(self):
+        self.rect.move_ip(-self.speed, 0)
+        if self.rect.right < 0:
+            self.kill()
 
-# Main event loop, contains everything that has to stay infinitely consistent
+
+class Mob(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((30, 40))
+        self.image.fill(RED)
+        self.rect = self.image.get_rect(
+            center=(
+                random.randint(SCREEN_WIDTH + 20, SCREEN_WIDTH + 100),
+                random.randint(0, SCREEN_HEIGHT)
+            )
+        )
+        self.speed = random.randint(5, 10)
+
+    def update(self):
+        self.rect.move_ip(-self.speed, 0)
+        if self.rect.right < 0:
+            self.rect = self.image.get_rect(
+                center=(
+                    random.randint(SCREEN_WIDTH + 20, SCREEN_WIDTH + 100),
+                    random.randint(0, SCREEN_HEIGHT)
+                )
+            )
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(os.path.join('../images', 'chinchilla_sprite_light.png'))
+        self.rect = self.image.get_rect()
+        self.rect.top = 25
+        self.rect.bottom = 320
+        self.speedx = 1
+        self.speedy = 1
+
+    def update(self):
+        self.speedx = 0
+        self.speedy = 0
+        userinput = pygame.key.get_pressed()
+        if userinput[pygame.K_LEFT]:
+            self.speedx = -player_speed
+        if userinput[pygame.K_RIGHT]:
+            self.speedx = +player_speed
+        if userinput[pygame.K_UP]:
+            self.speedy = -player_speed
+        if userinput[pygame.K_DOWN]:
+            self.speedy = +player_speed
+
+        self.rect.x += self.speedx
+        self.rect.y += self.speedy
+        if self.rect.right > 800:
+            self.rect.right = 800
+        if self.rect.left < 0:
+            self.rect.left = 0
+
+        # Player stays within the screen boundaries
+        if self.rect.x < 0:
+            self.rect.x = 0
+        elif self.rect.x > 736:
+            self.rect.x = 736
+        elif self.rect.y < 0:
+            self.rect.y = 0
+        # Value of 536 = height of screen - height of sprite (600px - 64px)
+        elif self.rect.y > 536:
+            self.rect.y = 536
+
+    def shoot(self):
+        bullet = Bullet(self.rect.centerx, self.rect.top)
+        all_sprites.add(bullet)
+        bullets.add(bullet)
+
+
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((30, 10))
+        self.image.fill(YELLOW)
+        self.rect = self.image.get_rect()
+        self.rect.bottom = y + 25
+        self.rect.centerx = x + 33
+        self.speedx = +10
+
+    def update(self):
+        self.rect.x += self.speedx
+        # kill if it moves off the top of the screen
+        if self.rect.bottom < 0:
+            self.kill()
+
+
+all_sprites = pygame.sprite.Group()
+mobs = pygame.sprite.Group()
+bullets = pygame.sprite.Group()
+player = Player()
+all_sprites.add(player)
+for i in range(8):
+    m = Mob()
+    all_sprites.add(m)
+    mobs.add(m)
+
+
+def quit_game():
+    global run
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                player.shoot()
+
+
 running = True
 while running:
     title()
     sound_maker()
+    CANVAS.blit(HIGH_SCORE, HIGH_SCORE_RECT)
     if more_button.draw(CANVAS):
         running = True
         while running:
@@ -122,19 +250,14 @@ while running:
                     running = False
 
             pygame.display.update()
-    # if high_score_button.draw(CANVAS):
-       # print("NO highscore")
-    if start_button.draw(CANVAS):
 
+    if start_button.draw(CANVAS):
         # Player sprite
         player_sprite(player_x, player_y)
-
-
-        # Main event loop, contains everything that has to stay infinitely consistent
-        running = True
+        run = True
         scroll = 0
-        while running:
-
+        while run:
+            quit_game()
             # Gets drawn first
             # Background image and coordinates of image appearance
             CANVAS.blit(BACKGROUND, (0, 0))
@@ -144,6 +267,11 @@ while running:
             scroll -= 10
             if abs(scroll) > bg_width:
                 scroll = 0
+
+            test = 0
+            if test == 0:
+                get_high_score()
+                high_score_main()
 
             # String formatting to format in leading zeros
             output_time = "Score {0}".format(total_seconds)
@@ -165,92 +293,44 @@ while running:
             enemySprites.update(CANVAS)
             enemy_timer += 1
             if enemy_timer == 30:
-                enemySprites.add(Upper(random.randint(7, 12)))
-                enemySprites.add(Lower(random.randint(7, 12)))
-            elif enemy_timer >= 50:
+                enemySprites.add(Upper(random.randint(6, 12)))
+                enemySprites.add(Lower(random.randint(6, 12)))
+            elif enemy_timer >= 60:
                 enemySprites.add(BaseEnemy(random.randint(8, 12)))
                 enemy_timer = 0
 
+            # Increase spawn frequency of existing timer
+            if total_seconds >= 200:
+                enemy_timer += 1
+
             # Time-triggered enemy spawning
-            if total_seconds >= 90:
+            if total_seconds >= 400:
                 bouncer_enemy_timer += 1
                 if bouncer_enemy_timer == 90:
                     enemySprites.add(Bouncer(10))
                     bouncer_enemy_timer = 0
 
-            # Increase spawn frequency of existing timer
-            if total_seconds >= 400:
-                enemy_timer += 1
+            pygame.time.delay(30)
+            all_sprites.update()
 
-            for event in pygame.event.get():
-                if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-                    running = False
+            hits = pygame.sprite.groupcollide(mobs, bullets, True, True)
+            for hit in hits:
+                m = Mob()
+                all_sprites.add(m)
+                mobs.add(m)
+            hit = pygame.sprite.groupcollide(enemySprites, bullets, True, True)
 
-                if event.type == pygame.QUIT:
-                    running = False
+            hits = pygame.sprite.spritecollide(player, mobs, False)
+            if hits:
+                run = False
+                running = False
 
-                # Checks whether keystroke is left, right, up or down when pressed
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_LEFT:
-                        player_x_change = -player_speed
-                    if event.key == pygame.K_RIGHT:
-                        player_x_change = player_speed
-                    if event.key == pygame.K_UP:
-                        player_y_change = -player_speed
-                    if event.key == pygame.K_DOWN:
-                        player_y_change = player_speed
-                    if event.key == pygame.K_SPACE:
-                        if bullet_state == "ready":
-                            bulletX = player_x
-                            bulletY = player_y
-                            fire_bullet(bulletY, bulletX)
-
-                if event.type == pygame.KEYUP:
-                    if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT \
-                            or event.key == pygame.K_UP or event.key == pygame.K_DOWN:
-                        player_x_change = 0
-                        player_y_change = 0
-
-            # Diagonal movement makes sprite disappear in corners
-
-            # Value of 736 = width of screen - width of sprite (800px - 64px)
-            if player_x < 0:
-                player_x = 0
-            elif player_x > 736:
-                player_x = 736
-            elif player_y < 0:
-                player_y = 0
-            # Value of 536 = height of screen - height of sprite (600px - 64px)
-            elif player_y > 536:
-                player_y = 536
-
-            if bulletX >= 800:
-                bullet_state = "ready"
-
-            if bullet_state == "fire":
-                fire_bullet(bulletX, bulletY)
-                bulletX += bulletX_change
-
-
-            '''
-            # This tracks the player's coordinates
-            print({player_x})
-            print({player_y})
-            print (total_seconds)
-            '''
-
-            test = 0
-            if test == 0:
-                get_high_score()
-                high_score_main()
-
-            player_x += player_x_change
-            player_y += player_y_change
-            player_sprite(player_x, player_y)
+            hit = pygame.sprite.spritecollide(player, enemySprites, False)
+            if hit:
+                run = False
+                running = False
+            all_sprites.draw(screen)
             pygame.display.update()
-
-            # Clock ticking to the game speed
-            clock.tick(frame_rate)
 
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
@@ -260,3 +340,15 @@ while running:
             running = False
 
     pygame.display.update()
+
+if run == False:
+    endgame = True
+    while endgame:
+        Game_Over()
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                endgame = False
+
+            if event.type == pygame.QUIT:
+                endgame = False
+        pygame.display.update()
